4e290d39aff04c5158942e8394dd23c214abcc00
Ahmed Gamal <agsalamh@gmail.com>
2022-04-22 07:29:34 +0200

    feat: add base model setup

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    '''Base setup for all model'''
+    id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
