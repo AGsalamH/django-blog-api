3ac736a913a63f535017502772c128b527bb5167
Ahmed Gamal <agsalamh@gmail.com>
2022-04-22 07:53:33 +0200

    feat(posts): add Post model and Serializer

---
+++ blog/posts/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+from blog.posts.models import Post
+
+
+UserModel = get_user_model()
+
+class CreatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('username',)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    creator = CreatorSerializer(read_only=True)
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'content', 'creator', 'created_at', 'updated_at')
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from blog.core.models import BaseModel
 
 # Create your models here.
+
+class Post(BaseModel):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
