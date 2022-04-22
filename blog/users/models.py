7e73f072e8f7b61871cb951e8f9f4664c61a9df2
Ahmed Gamal <agsalamh@gmail.com>
2022-04-23 01:13:13 +0200

    feat: add custom user model

--- blog/users/models.py
+++ blog/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
+
+from blog.core.models import BaseModel
 
 # Create your models here.
+
+class User(AbstractUser, BaseModel):
+    '''All Users are represented by this model'''
+    pass
