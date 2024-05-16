42c0dd4feb8f42e9652ac4da2ef17befa350014e
Ahmed Gamal <35201159+AGsalamH@users.noreply.github.com>
2024-05-17 02:56:52 +0300

    docs: Update models.py docs

--- blog/core/models.py
+++ blog/core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class BaseModel(models.Model):
-    '''Base setup for all model'''
+    '''Base UUID & Timestamped abstract model for all models to inherit from'''
     id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
